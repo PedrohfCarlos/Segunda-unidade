100d0ca32a78067c277cafa931a247a724152a82
Pedro Henrique Filgueira Carlos <128182599+PedrohfCarlos@users.noreply.github.com>
2024-02-06 00:11:36 +0000

    TAD

--- atividade_em_aula/aluno.h
+++
@@ -1,4 +0,0 @@
-/*definiçao de novo tipo: aluno */
-typedef struct aluno Aluno;
-/*função que alocar menoria para uma stuct alun, recebe os dados via tlecado e retorna um ponteiro parta Aluno*/
-Aluno * recebe_dados(void);
--- atividade_em_aula/main1.c
+++
@@ -1,6 +0,0 @@
-#include "aluno.h"
-int main(void){
-    Aluno *aluno;
-    recebe_dados();
-    return 0;
-}
